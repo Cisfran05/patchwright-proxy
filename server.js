52bb490f49fd10a8817b919e5ad6cf2d559eadde
Cisfran <cisfran05@wearehackerone.com>
2026-03-13 12:29:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -381,7 +381,7 @@ app.options("/api/*", (req, res) => {
 //
 // Start server
 //
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Proxy running on port ${PORT}`);
@@ -392,6 +392,7 @@ app.listen(PORT, "0.0.0.0", () => {
 
 
 });*/
+
 
 
 
